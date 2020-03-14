5745fe9f9065f12a2ee8cdd7e4295b40a8a6d572
Josh McKee <strangegraphs@gmail.com>
2020-03-14 11:00:12 -0600

    added missing files due to an error in git/visual studio

---
+++ ArithmeticLibrary/Arithmetic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ArithmeticLibrary
+{
+    public class Arithmetic
+    {
+        public int AddTwoNumbers(int firstNumber, int secondNumber)
+        {
+            int result = 0;
+
+            //Add addition code here.
+            result = firstNumber + secondNumber;
+
+            return result;
+        }
+
+        public int SubtractSecondNumberFromFirstNumber(int firstNumber, int secondNumber)
+        {
+            int result = 0;
+
+            //Add subtraction code here.
+            result = firstNumber - secondNumber;
+
+            return result;
+        }
+
+        public int MultiplyTwoNumbers(int firstNumber, int secondNumber)
+        {
+            int result = 0;
+
+            //Add multiplication code here.
+            result = firstNumber * secondNumber;
+
+            return result;
+        }
+
+        public int DivideFirstNumberBySecondNumber(int firstNumber, int secondNumber)
+        {
+            int result = 0;
+
+            //Add multiplication code here.
+            result = firstNumber / secondNumber;
+
+            return result;
+        }
+    }
+}
